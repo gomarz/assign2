24aacc4da02202e5f3df8a32755a935cbc8ad048
gomarz <bra2091589@gmail.com>
2019-10-04 21:19:20 -0700

    test

---
+++ src/cse360assign2/AddingMachine.java
@@ -0,0 +1,51 @@
+package cse360assign2;
+
+/**
+ * asdasdsadsadadadsadadsadadasdadsadsadadasd
+ * @author Solids
+ *
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * Return the total from the adding machine
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	 *  add an int to the <b>AddingMachine</b>
+	 * @param value The int to be added
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtract the int from the AddingMachine
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Return the string representation of the AddingMachine
+	 * 
+	 */
+	public String toString () {
+		return "";
+	}
+	/**
+	 * asdfgadasdsadsakfkfkfkfk
+	 */
+	public void clear() {
+	
+	}
+}
